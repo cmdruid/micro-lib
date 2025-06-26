bf27917508b3e6f98ee54108f48d9e2ea4a13cc6
cmdruid <cscottdev@protonmail.com>
2025-06-26 16:43:46 -0500

    added hash methods

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,13 +1,19 @@
 import { Buff, Buffable } from '@vbyte/buff'
 
-import { ripemd160 as r160 } from '@noble/hashes/legacy'
-import { sha256 as s256 }    from '@noble/hashes/sha2'
+import { hmac as noble_hmac }      from '@noble/hashes/hmac'
+import { pbkdf2 as noble_pbkdf2 }  from '@noble/hashes/pbkdf2'
+import { ripemd160 as noble_r160 } from '@noble/hashes/legacy'
+
+import {
+  sha256 as noble_s256,
+  sha512 as noble_s512
+} from '@noble/hashes/sha2'
 
 export function hash160 (
   ...input : Buffable[]
 ) : Buff {
   const buffer = Buff.join(input)
-  const digest = r160(s256(buffer))
+  const digest = noble_r160(noble_s256(buffer))
   return new Buff(digest)
 }
 
@@ -15,7 +21,7 @@ export function sha256 (
   ...input : Buffable[]
 ) : Buff {
   const buffer = Buff.join(input)
-  const digest = s256(buffer)
+  const digest = noble_s256(buffer)
   return new Buff(digest)
 }
 
@@ -23,7 +29,7 @@ export function hash256 (
   ...input : Buffable[]
 ) : Buff {
   const buffer = Buff.join(input)
-  const digest = s256(s256(buffer))
+  const digest = noble_s256(noble_s256(buffer))
   return new Buff(digest)
 }
 
@@ -40,4 +46,48 @@ export function hash340 (
   const data = input.map(e => new Buff(e))
   const pimg = Buff.join([ htag, ...data ])
   return sha256(pimg)
+}
+
+export function hmac256 (
+  key      : Buffable,
+  ...input : Buffable[]
+) : Buff {
+  const mask   = new Buff(key)
+  const buffer = Buff.join(input)
+  const digest = noble_hmac(noble_s256, mask, buffer)
+  return new Buff(digest)
+}
+
+export function hmac512 (
+  key      : Buffable,
+  ...input : Buffable[]
+) : Buff {
+  const mask   = new Buff(key)
+  const buffer = Buff.join(input)
+  const digest = noble_hmac(noble_s512, mask, buffer)
+  return new Buff(digest)
+}
+
+export function pkdf256 (
+  secret   : Buffable,
+  salt     : Buffable,
+  options? : Partial<{ c: number, dk_len: number }>
+) : Buff {
+  const { c = 1, dk_len = 32 } = options ?? {}
+  const sec_u8  = new Buff(secret)
+  const salt_u8 = new Buff(salt)
+  const digest  = noble_pbkdf2(noble_s256, sec_u8, salt_u8, { c, dkLen: dk_len })
+  return new Buff(digest)
+}
+
+export function pkdf512 (
+  secret   : Buffable,
+  salt     : Buffable,
+  options? : Partial<{ c: number, dk_len: number }>
+) : Buff {
+  const { c = 1, dk_len = 32 } = options ?? {}
+  const sec_u8  = new Buff(secret)
+  const salt_u8 = new Buff(salt)
+  const digest  = noble_pbkdf2(noble_s512, sec_u8, salt_u8, { c, dkLen: dk_len })
+  return new Buff(digest)
 }
